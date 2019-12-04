455f8d5fe9cfe9fc37641ea3088b7b94c5c6140b
gz <l2241883@gmail.com>
2019-12-04 21:17:27 +0800

    fix name

--- src/locales/en-US/index.js
+++ src/locales/en-US/index.js
@@ -2,7 +2,7 @@ export default {
   'app.components.loadableLoading': 'Page Loading',
   'app.layout.basicLayout.header.logout': 'Sign out',
   'app.menu.dashboard': 'dashboard',
-  'app.dashboard.welcome': 'Welcome to the DV Cover.',
+  'app.dashboard.welcome': 'Welcome to the Yellow Chicken.',
   'app.menu.tag.fold': 'fold',
   'app.menu.tag.expand': 'expand',
 }
--- src/locales/zh-CN/index.js
+++ src/locales/zh-CN/index.js
@@ -2,7 +2,7 @@ export default {
   'app.components.loadableLoading': '页面加载中',
   'app.layout.basicLayout.header.logout': '退出',
   'app.menu.dashboard': '首页',
-  'app.dashboard.welcome': '欢迎使用DV可视化门户。',
+  'app.dashboard.welcome': '欢迎使用Yellow Chicken。',
   'app.menu.tag.fold': '折叠',
   'app.menu.tag.expand': '展开',
 }
